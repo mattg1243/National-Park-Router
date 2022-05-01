a72ddc47043ea9ddfb28688721f48cae7f661d80
Matt Gallucci <mattgallucci97@gmail.com>
2022-05-01 11:41:22 -0700

    initial commit; reusing some code from dijsktras lab

---
+++ map.cpp
@@ -0,0 +1,32 @@
+// listing all the parks
+// 0 - Yosemite
+// 1 - Death Valley
+// 2 - Joshua Tree
+// 3 - Channel Islands
+// 4 - Redwoods
+// 5 - Point Reyes
+// 6 - Sequoia & Kings Canyon
+// 7 - Lassen
+// 8 - Pinnacles
+
+// hard coding the graph
+int map[9][9] = {
+  // from Yosemite
+  {0, 401, 387, 293, 553, 201, 143, 324, 216},
+  // from Death Valley
+  {},
+  // from Joshua Tree
+  {},
+  // from Channel Islands
+  {},
+  // from Redwoods
+  {},
+  // from Point Reyes
+  {},
+  // from Sequoia
+  {},
+  // from Lassen
+  {},
+  // from Pinnacles
+  {},
+}

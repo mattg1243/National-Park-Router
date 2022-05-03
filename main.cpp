bbab701281940d17d89c6691a0612aac50bbc3d3
Matt Gallucci <mattgallucci97@gmail.com>
2022-05-02 17:26:18 -0700

    cli test in main, refactor in map

--- main.cpp
+++ main.cpp
@@ -1,21 +1,12 @@
 #include <iostream>
-#include "park.hpp"
+#include "navigator.hpp"
 
 using namespace std;
 
 int main() {
 
-// instantiate all 9 parks in California
-Park* Yosemite = new Park(0, "Yosemite");
-Park DeathValley = Park(1, "Death Valley");
-Park JoshuaTree = Park(2, "Joshua Tree");
-Park ChannelIslands = Park(3, "Channel Islands");
-Park Redwoods = Park(4, "Redwoowds");
-Park PointReyes = Park(5, "Point Reyes");
-Park Sequoias = Park(6, "Sequoias");
-Park Lassen = Park(7, "Lassen");
-Park Pinnacles = Park(8, "Pinnacles");
-
-cout << "Yosemite object test: \nkey - " << Yosemite->key << "\nname - " << Yosemite->name << "\n";
+  // create a navigator object and run the cli
+  Navigator n = Navigator();
+  n.startCLI();
 
 }

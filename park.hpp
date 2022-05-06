bf7a4b87289ec1d2ecfe92bc982d13fda81074b2
Matt Gallucci <mattgallucci97@gmail.com>
2022-05-06 01:23:38 -0700

    added overloaded operators to work with std functions

--- park.hpp
+++ park.hpp
@@ -9,13 +9,20 @@ class Park {
   std::string name;
   // an adjacency list representing the distances 
   // the index i = this.key will be set to 0 for consistency purposes
-  int (&distances)[9];
+  //int (&distances)[9];
   // the park that is visisted before this parks
   // along the optimal path
   Park* predV;
   // constructor
-  Park(int k, std::string n, int (&d)[9]) : distances(d) {
+  Park(int k, std::string n) {
     key = k; name = n; predV = nullptr; 
   }
+  // overloaded equlity operator
+  bool operator==(Park& other) const {
+    if (key == other.key) {return true;}
+    else {return false;}
+  }
+  bool friend operator<(const Park& r, const Park& l) {
+    if (r.key < l.key) {return true;} else {return false;}}
   
 };

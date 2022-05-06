ff878f3ebaafd176446a3f1542d0c3a766753e6b
Matt Gallucci <mattgallucci97@gmail.com>
2022-05-05 22:41:17 -0700

    park constructor now accepts adjacency list as an argument

--- park.hpp
+++ park.hpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 // a class for national parks
 class Park {
   public:
@@ -8,10 +9,13 @@ class Park {
   std::string name;
   // an adjacency list representing the distances 
   // the index i = this.key will be set to 0 for consistency purposes
-  int distances[9];
+  int (&distances)[9];
   // the park that is visisted before this parks
   // along the optimal path
   Park* predV;
-  Park(int k, std::string n) { key = k; name = n; distances[key] = 0; predV = nullptr; }
+  // constructor
+  Park(int k, std::string n, int (&d)[9]) : distances(d) {
+    key = k; name = n; predV = nullptr; 
+  }
   
 };

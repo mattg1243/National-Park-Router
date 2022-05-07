97712b3709d0f12ca1f6952dfc16ae39792a333e
Matt Gallucci <mattgallucci97@gmail.com>
2022-05-07 13:48:34 -0700

    deleted unused code

--- park.hpp
+++ park.hpp
@@ -7,9 +7,6 @@ class Park {
   int key;
   // the name of the park
   std::string name;
-  // an adjacency list representing the distances 
-  // the index i = this.key will be set to 0 for consistency purposes
-  //int (&distances)[9];
   // the park that is visisted before this parks
   // along the optimal path
   Park* predV;
